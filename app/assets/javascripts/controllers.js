0f01b7b8b4b9800fbc0fa52760e498af1c8aa758
danwheeler <da-wheeler@hotmail.co.uk>
2015-12-18 14:27:57 +0000

    json call

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -20,6 +20,62 @@ autopairControllers.controller('AddTestController', [function() {
 
 }]);
 
-autopairControllers.controller('ApplicantController', [function($resource) {
+autopairControllers.controller('ApplicantController', ['$resource', function($resource) {
+	var self = this;
 
-}]);
+	// self.applicants = [
+ //    {
+ //    	"id": "1",
+ //      "name": "tansaku",
+ //      "email": "https://avatars.githubusercontent.com/u/30216?v=3",
+ //      "test_id": 1,
+ //      "score": 2
+ //    }, 
+ //    {
+ //    	"id": 2,
+ //      "name": "tim",
+ //      "email": "one@one.com",
+ //      "test_id": 5,
+ //      "score": 3
+ //    }
+ //  ];
+
+  self.applicants = return $resource("app/assets/controllers/applicants_controller/applicants/:id", { id: "@id" },
+    {
+      'create':  { method: 'POST' },
+      'index':   { method: 'GET', isArray: true },
+      'show':    { method: 'GET', isArray: false },
+      'update':  { method: 'PUT' },
+      'destroy': { method: 'DELETE' }
+    }
+  );
+ condole.log(self.applicants);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
