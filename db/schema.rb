3b10e4610eac6e3c71878993f1f357e383176a46
Yev Dyko <evgeny.dyko@gmail.com>
2016-01-06 02:27:37 +0300

    Added associating tests and questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104164804) do
+ActiveRecord::Schema.define(version: 20160105164727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,10 @@ ActiveRecord::Schema.define(version: 20160104164804) do
     t.string   "rspec_test"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "test_id"
   end
+
+  add_index "questions", ["test_id"], name: "index_questions_on_test_id", using: :btree
 
   create_table "tests", force: :cascade do |t|
     t.string   "description"
@@ -39,4 +42,5 @@ ActiveRecord::Schema.define(version: 20160104164804) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "questions", "tests"
 end
